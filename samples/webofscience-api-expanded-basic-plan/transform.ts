1c7584e2e2c70fa831bd2b40b67c106a7db2b5ec
Mailson Teles Borges <telesborges@outlook.com>
2024-10-06 18:59:08 +0100

    Adding WebOfScience sample

---
+++ samples/webofscience-api-expanded-basic-plan/transform.ts
@@ -0,0 +1,16 @@
+import { HyperledgerFabricGolangFactory } from './../../src/factories/hyperledger-fabric-golang.factory';
+import { readFileSync, writeFileSync } from 'fs';
+import { execSync } from 'child_process';
+
+const contract = 'webofscience-api-expanded-basic-plan';
+
+const cwd = `./samples/${contract}`;
+
+const content = readFileSync(`${cwd}/${contract}.jabuti`, 'utf-8');
+const files = new HyperledgerFabricGolangFactory().transform(content);
+
+writeFileSync(`${cwd}/${contract}.go`, files.content);
+writeFileSync(`${cwd}/go.mod`, files.mod);
+writeFileSync(`${cwd}/${contract}.fabric`, files.spydra);
+
+execSync('go mod tidy', { cwd });
